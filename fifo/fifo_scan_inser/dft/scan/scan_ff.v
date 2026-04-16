7ad6a59c067da70c9953bc1a58f608bffb7b2bbf
vijay <vijaykrishnanunni11@gmail.com>
2026-04-16 19:26:41 +0530

    0

---
+++ fifo/fifo_scan_inser/dft/scan/scan_ff.v
@@ -0,0 +1,23 @@
+module scan_ff (
+    input  wire clk,
+    input  wire rst_n,
+    input  wire scan_en,
+    input  wire si,
+    input  wire d,
+    output reg  q,
+    output wire so
+);
+
+    wire d_mux;
+
+    assign d_mux = (scan_en) ? si : d;
+    assign so    = q;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            q <= 1'b0;
+        else
+            q <= d_mux;
+    end
+
+endmodule
